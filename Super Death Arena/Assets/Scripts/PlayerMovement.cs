1c91143b84afe89ee7d1357014144e444284aaad
Daniel Udal (s5111564) <s5111564@bournemouth.ac.uk>
2020-03-11 19:40:21 +0000

    player controller added

---
+++ Super Death Arena/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 0.5f;
+    float xMove;
+    float yMove;
+    CharacterController character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xMove = 0.0f;
+        yMove = 0.0f;
+        character = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xMove = Input.GetAxis("Horizontal");
+        yMove = Input.GetAxis("Vertical");
+        character.Move((new Vector3(xMove, 0.0f, yMove)) * speed);
+    }
+}
