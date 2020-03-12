0dffa89147d61ca0ed73443f44c1677ac0f51572
AgentArachnid <46264591+AgentArachnid@users.noreply.github.com>
2020-03-12 14:51:38 +0000

    Add in player character

--- Super Death Arena/Assets/Scripts/PlayerMovement.cs
+++ Super Death Arena/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,13 @@ public class PlayerMovement : MonoBehaviour
     float yMove;
     CharacterController character;
 
+    public bool dead;
+    public float health = 100;
+    public float maxHealth = 100;
+
+
+    public Animator animCont;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +27,46 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        dead = (health / maxHealth) <= 0;
+        animCont.SetBool("Dead", dead);
         xMove = Input.GetAxis("Horizontal");
         yMove = Input.GetAxis("Vertical");
-        character.Move((new Vector3(xMove, 0.0f, yMove)) * speed);
+        Vector3 velocity = new Vector3(xMove, 0.0f, yMove) * speed;
+        animCont.SetFloat("Blend", velocity.magnitude);
+        character.Move(velocity);
+
+        
+
+
+
+        // This is for testing purposes. Could easily substitute the actual controllers later
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Attack");
+            animCont.SetBool("Blocking", false);
+            animCont.SetBool("Attacking", true);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Block");
+            animCont.SetBool("Attacking", false);
+            animCont.SetBool("Blocking", true);
+
+        }
+
+    }
+    //Animation events to prevent looping
+    public void endAttack()
+    {
+        Debug.Log("End of attack");
+        animCont.SetBool("Attacking", false);
+    }
+
+
+    public void endBlock()
+    {
+        Debug.Log("End Block");
+        animCont.SetBool("Blocking", false);
     }
 }
