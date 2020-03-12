3428b0e1bd04f88acca7fdf6f61f70e795448d47
AgentArachnid <46264591+AgentArachnid@users.noreply.github.com>
2020-03-12 19:10:29 +0000

    AI, player character, and Animation Controller

---
+++ Super Death Arena/Assets/Scripts/AI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class AI : MonoBehaviour
+{
+    public Animator animCont;
+    public PlayerMovement playerRef;
+    public NavMeshAgent agent;
+    public float HP;
+    public bool dead;
+
+
+    public float attackThreshold;
+    private bool attacking;
+
+    public Vector3 destination;
+
+    void Update()
+    {
+
+        dead = HP <= 0;
+        animCont.SetBool("Dead", dead);
+        agent.enabled = !dead;
+        destination = playerRef.gameObject.transform.position;
+        if (agent.enabled = true)
+        {
+            agent.SetDestination(destination);
+        }
+        attacking = ((destination - agent.transform.position).magnitude <= attackThreshold);
+        animCont.SetBool("Attacking", attacking);
+        agent.enabled = !attacking;
+
+
+        if (dead)
+        {
+            Destroy(gameObject, 2f);
+        }
+
+    }
+
+
+}
+
